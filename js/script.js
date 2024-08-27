76c8cb0bd26ea079ea60c44dfe72a591c4d0c945
Abrahan Rifat <166337684+abrahanrifat1100@users.noreply.github.com>
2024-08-27 16:35:49 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,38 @@
+//ex1
+function textShow(){
+	document.getElementById('sh').style.display='block';
+}
+function textHide(){
+	document.getElementById('sh').style.display='none';
+}
+//ex2
+function textChange(){
+	document.getElementById('tc').innerHTML="welcome to wwr"
+}
+//ex3
+function showDate(){
+	document.getElementById('sd').innerHTML=Date();
+}
+//ex4
+function BulbOn(){
+	document.getElementById('bulb').src='images/on.gif';
+}
+function BulbOff(){
+	document.getElementById('bulb').src='images/off.gif';
+}
+//ex5
+function EmojiA(){
+	document.getElementById('em').src='images/sad.png';
+}
+function EmojiB(){
+	document.getElementById('em').src='images/smile.png';
+}
+//ex6
+function ChangeCss(){
+	document.getElementById('hw').style.color='red';
+}
+//ex7
+function fontSize(){
+	document.getElementById('fs').style.fontSize='35px';
+}
+
